815ae4e1da9d93dadc30524671ed1ef7eb149c74
Victor Venturini <Victormventurini@gmail.com>
2021-03-20 01:51:04 -0300

    add funcionalidade de escolher quantidade de idades

--- calculo_media.py
+++ calculo_media.py
@@ -1,10 +1,12 @@
 lista_idades = []
-for i in range(0,10):
+quantidade_idades = int(input("Quantas idades serâo calculadas: "))
+
+for i in range(0,quantidade_idades):
     idade = int(input("Digite a idade: "))
     lista_idades.append(idade)
 
 soma_idades = sum(lista_idades)
 
-media_idades = soma_idades/10
+media_idades = soma_idades/quantidade_idades
 
 print("A media das idades é:" + str(media_idades))
